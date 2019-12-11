6a5af9c22e59f0cb58ecbf2266df321c3ef28191
vutr <me@vutr.io>
2019-12-12 03:05:38 +0700

    Add some more log

--- generators/app/templates/main.py
+++ generators/app/templates/main.py
@@ -6,6 +6,11 @@ CONFIG = load_config()
 
 def main():
     logger.info('Hello World! This is project: <%= project %>')
+    logger.warning('- To run app from `main.py`, use `pipenv run app`')
+    logger.debug('- To test the app, use `pipenv run test`')
+    logger.error(
+        "- Submit your issues to `https://github.com/vutran1710/Pywork/issues` if needed"
+    )
 
 
 if __name__ == '__main__':
