f340230f05ab585527831aae4668e0d71bd4821e
vutr <me@vutr.io>
2020-01-26 09:52:11 +0700

    fix ignore

--- generators/app/templates/fastapi/apis/demo.py
+++ generators/app/templates/fastapi/apis/demo.py
@@ -4,10 +4,10 @@ from conn.redis import RedisClient
 from utils import CONFIG
 
 router = APIRouter()
-redis_db = RedisClient(CONFIG)
+redis = RedisClient(CONFIG)
 
 
 @router.get("/get-something", response_model=SomePayload)
 async def get_best_posts(limit: int):
-    payload = await redis_db.do_some_redis_shit(limit)
+    payload = await redis.do_some_redis_shit(limit)
     return payload
