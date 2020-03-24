29e57db2265bf23ea10e2efca28b5c4ac88f9c17
vutr <me@vutr.io>
2020-03-25 00:58:59 +0700

    fix api handler init singleton class

--- generators/app/templates/fastapi/apis/demo.py
+++ generators/app/templates/fastapi/apis/demo.py
@@ -1,13 +1,18 @@
+""" Demo API
+"""
 from fastapi import APIRouter
 from models import SomePayload
 from conn.redis import RedisClient
-from utils import CONFIG
 
 router = APIRouter()
-redis = RedisClient(CONFIG)
 
 
 @router.get("/get-something", response_model=SomePayload)
 async def get_best_posts(limit: int):
+    """ Get some thig
+    """
+    # NOTE: RedisClient is a singleton class,
+    # will return the early-inited instance
+    redis: RedisClient = RedisClient(...)
     payload = await redis.do_some_redis_shit(limit)
     return payload
