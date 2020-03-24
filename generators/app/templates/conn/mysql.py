b226e04be118335d9c215e8ada280ea6c9f3123c
vutr <me@vutr.io>
2020-03-25 01:03:51 +0700

    fix mysql

--- generators/app/templates/conn/mysql.py
+++ generators/app/templates/conn/mysql.py
@@ -1,33 +1,30 @@
+""" MySqlClient
+"""
+from typing import List, Any
 from pymysql import connect, cursors
 from pydantic import BaseModel
-
-from decorator import singleton
-
-
-class MySqlConfig(BaseModel):
-    MYSQL_HOST: str
-    MYSQL_USER: str
-    MYSQL_PWD: str
-    MYSQL_DB: str
+from helpers import Singleton
+from utils import AppConfig
 
 
-@singleton
-class MySqlClient:
+class MySqlClient(metaclass=Singleton):
     """MySqlClient
     """
-    def __init__(self, config: MySqlConfig):
+    def __init__(self, config: AppConfig):
         conn = connect(
-            host=config['MYSQL_HOST'],
-            user=config['MYSQL_USER'],
-            password=config['MYSQL_PWD'],
-            db=config['MYSQL_DB'],
+            host=config.MYSQL_HOST,
+            user=config.MYSQL_USER,
+            password=config.MYSQL_PWD,
+            db=config.MYSQL_DB,
             charset='utf8mb4',
             cursorclass=cursors.DictCursor,
         )
 
         self.conn = conn
 
-    def query_something(self, limit: int):
+    def query_something(self, limit: int) -> List[Any]:
+        """ Query anything
+        """
         with self.conn.cursor() as cursor:
             q = """
             SELECT post_id FROM some_table ORDER BY created_at DESC LIMIT %s
