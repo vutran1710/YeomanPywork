a959222ee3d060780b45c8f6c9ccd70112dcec9b
vutr <me@vutr.io>
2020-03-25 00:54:03 +0700

    fix decorators & mixin

--- generators/app/templates/fastapi/decorator.py
+++
@@ -1,7 +0,0 @@
-def singleton(aClass):
-    def onCall(*args, **kwargs):
-        if onCall.instance is None:
-            onCall.instance = aClass(*args, **kwargs)
-        return onCall.instance
-    onCall.instance = None
-    return onCall
